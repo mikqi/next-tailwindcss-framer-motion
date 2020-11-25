ef1da538cb99714425bb01c26ba92204f5bd2f03
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-11-25 18:24:56 +0700

    chore: update tailwindcss 2.0

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const Home: React.FC = () => (
     className="flex items-center justify-center min-h-screen overflow-hidden bg-gradient-to-r from-purple-400 via-pink-500 to-red-500"
   >
     <motion.div
-      className="bg-white h-40 w-40"
+      className="bg-white dark:bg-black h-40 w-40"
       animate={{
         scale: [1, 2, 2, 1, 1],
         rotate: [0, 0, 270, 270, 0],
