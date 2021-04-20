b742ea53bec652b04ca082f6f5363ae71d22bef7
Muhammad Rivki <muhammad.rivki@live.com>
2021-04-20 08:57:49 +0700

    chore: update dependencies (#8)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['plugin:react/recommended', 'prettier', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  mode: 'jit',
   darkMode: 'class', // "media" or "class"
   purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
   future: {
