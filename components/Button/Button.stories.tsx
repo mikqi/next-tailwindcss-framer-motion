ac835612910958df3a7c46e5aa1455070a07436c
Muhammad Rivki <muhammad.rivki@live.com>
2022-01-23 23:15:31 +0700

    chore: update dependencies

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -6,21 +6,21 @@ import { Button, ButtonProps } from './Button'
 export default {
   title: 'Example/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as Meta
 
 const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
-  primary: true,
+  variant: 'primary',
+  size: 'md',
   label: 'Button',
 }
 
 export const Secondary = Template.bind({})
 Secondary.args = {
+  variant: 'secondary',
+  size: 'md',
   label: 'Button',
 }
 
--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -5,15 +5,11 @@ export interface ButtonProps {
   /**
    * Is this the principal call to action on the page?
    */
-  primary?: boolean
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string
+  variant?: 'primary' | 'secondary' | 'white' | 'danger'
   /**
    * How large should the button be?
    */
-  size?: 'sm' | 'md' | 'lg'
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl'
   /**
    * Button contents
    */
@@ -22,33 +18,56 @@ export interface ButtonProps {
    * Optional click handler
    */
   onClick?: () => void
+
+  leftIcon?: React.FunctionComponent<React.ComponentProps<'svg'>>
+  rightIcon?: React.FunctionComponent<React.ComponentProps<'svg'>>
 }
 
 const buttonSize = {
-  sm: 'px-4 py-1 text-sm;',
-  md: 'px-5 py-2 text-sm',
-  lg: 'px-6 py-3 text-base',
+  xs: 'px-2.5 py-1.5 text-xs font-medium rounded',
+  sm: 'px-3 py-2 text-sm leading-4 rounded-md',
+  md: 'px-4 py-2 text-sm font-medium rounded-md',
+  lg: 'px-4 py-2 text-base font-medium rounded-md',
+  xl: 'px-6 py-3 text-base font-medium rounded-md',
+}
+
+const variants = {
+  primary: 'text-white bg-indigo-600 hover:bg-indigo-700',
+  secondary: 'text-indigo-700 bg-indigo-100 hover:bg-indigo-200',
+  white: 'text-gray-700 bg-white border border-gray-300 hover:bg-gray-50',
+  danger: 'text-red-700 bg-red-100 hover:bg-red-200 focus:ring-red-500',
 }
 
 /**
  * Primary UI component for user interaction
  */
 export const Button: React.FC<ButtonProps> = ({
-  primary = false,
+  variant = 'primary',
   size = 'md',
-  backgroundColor,
   label,
   children,
+  leftIcon,
+  rightIcon,
   ...props
 }) => {
   const classes = clsx(
-    'inline-block font-sans font-bold leading-none rounded-lg cursor-pointer',
+    'inline-flex items-center  border border-transparent font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
     buttonSize[size],
-    primary ? 'text-white bg-blue-500' : 'text-blue-500 bg-transparent border border-blue-500'
+    variants[variant]
   )
   return (
-    <button type="button" className={classes} style={{ backgroundColor }} {...props}>
+    <button type="button" className={classes} {...props}>
+      {leftIcon &&
+        React.createElement(leftIcon, {
+          className: size === 'xs' ? '-ml-0.5 mr-2 h-4 w-4' : '-ml-1 mr-2 h-5 w-5',
+        })}
+
       {label || children}
+
+      {rightIcon &&
+        React.createElement(rightIcon, {
+          className: size === 'xs' ? 'ml-2 -mr-0.5 h-4 w-4' : 'ml-2 -mr-1 h-5 w-5',
+        })}
     </button>
   )
 }
