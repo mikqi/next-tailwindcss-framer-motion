bf4509f80249af633dd3dd68ceecfff2bb55f379
Muhammad Rivki <Muhammad.Rivki@live.com>
2022-11-05 07:44:55 +0700

    chore: fix linter

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -60,14 +60,16 @@ export const Button: React.FC<ButtonProps> = ({
     <button type="button" className={classes} {...props}>
       {leftIcon &&
         React.createElement(leftIcon, {
-          className: size === 'xs' ? '-ml-0.5 mr-2 h-4 w-4' : '-ml-1 mr-2 h-5 w-5',
+          className:
+            size === 'xs' ? '-ml-0.5 mr-2 h-4 w-4' : '-ml-1 mr-2 h-5 w-5',
         })}
 
       {label || children}
 
       {rightIcon &&
         React.createElement(rightIcon, {
-          className: size === 'xs' ? 'ml-2 -mr-0.5 h-4 w-4' : 'ml-2 -mr-1 h-5 w-5',
+          className:
+            size === 'xs' ? 'ml-2 -mr-0.5 h-4 w-4' : 'ml-2 -mr-1 h-5 w-5',
         })}
     </button>
   )
