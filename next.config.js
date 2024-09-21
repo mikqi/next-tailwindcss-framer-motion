5d5378af840c24a95f2f795373d42c2979a810ac
Muhammad Rivki <Muhammad.Rivki@live.com>
2024-09-21 18:43:18 +0700

    chore: skip lint on build process next and add biome before build

--- next.config.js
+++ next.config.js
@@ -4,4 +4,9 @@ const { i18n } = require('./next-i18next.config')
 module.exports = {
   swcMinify: true,
   i18n,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
