ce9a1e686c2dc152dc6d4bd2e5b4169001c8e42d
Muhammad Rivki <Muhammad.Rivki@live.com>
2024-09-21 17:38:25 +0700

    chore: replace jest using vitest

--- jest.setup.js
+++
@@ -1,6 +0,0 @@
-import '@testing-library/jest-dom/extend-expect'
-
-global.window = {}
-global.window = global
-
-jest.mock('next/router', () => require('next-router-mock'))
---
+++ vitest.config.ts
@@ -0,0 +1,37 @@
+import path from 'path'
+import { defineConfig } from 'vitest/config'
+
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./vitest.setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules',
+        '.next',
+        'out',
+        'pages',
+        'utils/test/*.{ts,tsx,js}',
+        '.storybook',
+        '**/*.stories.*',
+        './*.{ts,tsx,js}',
+      ],
+    },
+    alias: {
+      '@': path.resolve(__dirname, './'),
+      '@/components': path.resolve(__dirname, './components'),
+      '@/utils': path.resolve(__dirname, './utils'),
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+})
