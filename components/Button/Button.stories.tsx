484cc58a11380d8bc4dfff5d7959f3bcda082d08
Muhammad Rivki <Muhammad.Rivki@live.com>
2024-09-21 18:24:36 +0700

    chore: upgrade nextjs version to v14

--- components/Button/Button.stories.tsx
+++ components/Button/Button.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Meta, Story } from '@storybook/react'
+import { Meta, StoryFn } from '@storybook/react'
 
 import { Button, ButtonProps } from './Button'
 
@@ -9,7 +9,7 @@ export default {
   component: Button,
 } as Meta
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />
+const Template: StoryFn<ButtonProps> = (args) => <Button {...args} />
 
 export const Primary = Template.bind({})
 Primary.args = {
