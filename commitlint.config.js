7aa760e9c7353e1c11df9a674c0968833cb7ecac
Muhammad Rivki <Muhammad.Rivki@live.com>
2025-07-05 19:30:07 +0700

    ci: Add commitlint rules for message body and subject
    Introduce rules to enforce a maximum line length for the commit message body and specify allowed casing conventions for the subject.
    
    - `body-max-line-length`: Sets the maximum line length for the commit body to 120 characters.
    - `subject-case`: Allows various casing options (sentence, start, pascal, upper, lower) for the commit subject.
    
    This improves consistency and readability of commit messages.

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,18 @@ module.exports = {
   rules: {
     //   TODO Add Scope Enum Here
     // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
+    'body-max-line-length': [2, 'always', 140],
+    'subject-case': [
+      2,
+      'always',
+      [
+        'sentence-case',
+        'start-case',
+        'pascal-case',
+        'upper-case',
+        'lower-case',
+      ],
+    ],
     'type-enum': [
       2,
       'always',
