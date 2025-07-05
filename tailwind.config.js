efb68717aaec9e8398aa38aa829dd2545eae5d4f
Muhammad Rivki <Muhammad.Rivki@live.com>
2025-07-05 18:59:45 +0700

    chore: update Tailwind CSS configuration to include app directory and enhance TypeScript settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   mode: 'jit',
   darkMode: 'class', // "media" or "class"
-  content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  content: ['./pages/**/*.tsx', './components/**/*.tsx', './app/**/*.tsx'],
   theme: {
     extend: {},
   },
